aeaa6c4336342f0a22f0b777d20edc24f31cc67c
MarlinaPuspitaDewi29 <56263544+MarlinaPuspitaDewi29@users.noreply.github.com>
2019-10-10 21:02:20 +0700

    menambahkan file mahasiswa menggunakan bootstrap

---
+++ phpmvc++/app/controllers/About.php
@@ -0,0 +1,24 @@
+<?php
+
+class About extends Controller
+{
+    public function index($nama = 'Marlina Pupsita Dewi', $umur = 20, $status = 'Mahasiswa')
+    {
+        $data['nama'] = $nama;
+        $data['umur'] = $umur;
+        $data['status'] = $status;
+        $data['judul'] = 'About';
+
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function page()
+    {
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+}
