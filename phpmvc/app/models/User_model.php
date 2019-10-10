f40f86a148d3ca743b7157d09108ce9d8e12c4fe
MarlinaPuspitaDewi29 <56263544+MarlinaPuspitaDewi29@users.noreply.github.com>
2019-10-10 13:26:27 +0700

    mengupload file phpmvc

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Marlina Puspita Dewi';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+
+
+    <h1 class="mt-4">About Me</h1>
+    <img src=" <?= BASEURL; ?> /img/1.jpg" alt="Marlina Puspita Dewi" width="200" class="rounded-circle shadow">
+    <p>Hallo, nama saya <?= $data['nama']; ?>, status saya sebagai <?= $data['status']; ?> 
+    di <?= $data['jurusan']; ?>.</p>
+</div>
